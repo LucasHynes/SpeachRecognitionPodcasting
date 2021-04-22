ce45c71afbaca7c429824eb5d0292c6dad9fa1b1
Lucas Hynes <65365981+LucasHynes@users.noreply.github.com>
2021-04-22 15:55:27 -0700

    Add files via upload

---
+++ team_setter.py
@@ -0,0 +1,38 @@
+import team as node
+
+def get_teams():
+
+    atl = ["Atlanta Hawks", ["Atlanta", "Hawks", "Georgia", "MLK"]]
+    bos = ["Boston Celtics", ["Boston", "Celtics", "Celtic", "Green"]]
+    bro = ["Brooklyn Nets", ["Brooklyn", "Nets", "Bedford", "New Jersey"]]
+    cha = ["Charlotte Hornets", ["Charlotte", "Hornets", "MJ"]]
+    chi = ["Chicago Bulls", ["Bulls", "Chicago", "deep dish"]]
+    cle = ["Cleveland Cavaliers", ["Cavs", "Sexland", "Cleveland", "Cavaliers", "sex land"]]
+    dal = ["Dallas Mavericks", ["Dallas", "Mavericks", "Mavs", "Cuban"]]
+    den = ["Denver Nuggets", ["Denver", "Nuggets", "nug", "colorado"]]
+    det = ["Detroit Pistons", ["Detroit", "Pistons", "Det", "motor city"]]
+    gsw = ["Golden State Warriors", ["Golden", "State", "Warriors", "San Francisco", "bay"]]
+    hou = ["Houston Rockets", ["Houston", "Rockets", "hou"]]
+    ind = ["Indiana Pacers", ["Indiana", "Pacers"]]
+    lac = ["LA Clippers", ["LA", "Clippers"]]
+    lal = ["Los Angeles Lakers", ["Los", "Angeles", "Lakers"]]
+    mem = ["Memphis Grizzlies", ["Griz", "Memphis", "Grizzlies", "Tennesee"]]
+    mia = ["Miami Heat", ["Miami", "Heat", "South Beach"]]
+    mil = ["Milwaukee Bucks", ["Milwaukee", "Bucks", "Fear", "deer"]]
+    min = ["Minnesota Timberwolves", ["Minnesota", "Timberwolves", "wolves"]]
+    nor = ["New Orleans Pelicans", ["New Orleans", "Pelicans", "Pels"]]
+    nyk = ["New York Knicks", ["New York", "Knicks", "Knickerbokers"]]
+    okc = ["Oklahoma City Thunder", ["OKC", "ok c", "ok see", "Oklahoma City", "Oklahoma", "Thunder"]]
+    orl = ["Orlando Magic", ["Orlando", "Majic"]]
+    phi = ["Philadelphia 76ers", ["Philly", "Philadelphia", "76", "76ers", "sixers"]]
+    pho = ["Phoenix Suns", ["Phoenix", "Suns", "Arizona", "Bright future"]]
+    por = ["Portland Trail_Blazers", ["Portland", "Trailblazers", "blazers", "rip city", "rose garden", "moda center", "Terry Stotts"]]
+    sac = ["Sacramento Kings", ["Sacramento", "Kings", "Sac", "sac town", "sactown"]]
+    san = ["San Antonio Spurs", ["San Antonio", "Spurs", "pop", "popavich"]]
+    tor = ["Toronto Raptors", ["Toronto", "Raptors", "Tampa", "Canada"]]
+    uta = ["Utah Jazz", ["Utah", "Jazz", "Salt Lake"]]
+    was = ["Washington Wizards", ["Washington", "Wizzards", "Wa", "D.C."]]
+
+    leage = [atl, bos, bro, cha, chi, cle, dal, den, det, gsw, hou, ind, lac, lal, mem, mia, mil, min, nor, nyk, okc, orl, phi, pho, por, sac, san, tor, uta, was]
+
+    return leage
